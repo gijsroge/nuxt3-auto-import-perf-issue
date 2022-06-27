c06f3f3cf771f4aca490de52457875f43211f15f
gijsroge <rogegijs@gmail.com>
2022-06-27 18:45:39 +0200

    1 file modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   components: false,
+  autoImports: { global: false, dirs: null },
   build: {
     transpile: [
       'naive-ui',
